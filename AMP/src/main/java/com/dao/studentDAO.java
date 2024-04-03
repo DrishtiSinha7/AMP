d59a220a458e1575abca093a5d2b478a5df59086
Drishti Sinha <112553763+DrishtiSinha7@users.noreply.github.com>
2024-04-03 09:28:57 +0530

    Add files via upload

---
+++ AMP/src/main/java/com/dao/studentDAO.java
@@ -0,0 +1,75 @@
+package com.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.entity.Student;
+
+public class studentDAO {
+
+    private Connection conn;
+
+    public studentDAO(Connection conn) {
+        super();
+        this.conn = conn;
+    }
+
+    public boolean addStudent(Student u) {
+        boolean f = false;
+        try {
+            String sql = "INSERT INTO student (name, email, password, phone) VALUES (?, ?, ?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, u.getName());
+            ps.setString(2, u.getEmail());
+            ps.setString(3, u.getPassword());
+            ps.setString(4, u.getPhone());
+            
+            int i = ps.executeUpdate();
+            if (i == 1) {
+                f = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+
+    public Student login(String em, String psw) {
+        Student u = null;
+        try {
+            String sql = "SELECT * FROM student WHERE email=? AND password=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, em);
+            ps.setString(2, psw);
+           
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                u = new Student();
+                u.setSid(rs.getInt(1));
+                u.setName(rs.getString(2));
+                u.setEmail(rs.getString(3));
+                u.setPassword(rs.getString(4));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return u;
+    }
+
+    public boolean validateStudent(String em, String psw) {
+        boolean isValid = false;
+        try {
+            String sql = "SELECT * FROM student WHERE email=? AND password=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, em);
+            ps.setString(2, psw);
+
+            ResultSet rs = ps.executeQuery();
+            isValid = rs.next();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return isValid;
+    }
+}
---
+++ AMP/src/main/java/com/entity/Student.java
@@ -0,0 +1,56 @@
+package com.entity;
+
+public class Student {
+
+	private int sid;
+	private String name;
+	private String email;
+	private String password;
+	private String phone;
+	
+
+	public Student(String name, String email, String password, String phone) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.phone = phone;
+		
+	}
+	public Student() {
+		super();
+		
+	}
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
